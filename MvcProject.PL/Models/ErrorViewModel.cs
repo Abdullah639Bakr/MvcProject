61b079392386dff72caa531a319803a27f5b507b
hamad <hamad@ABDULLAHBAKR>
2024-09-25 18:23:45 +0300

    Add project files.

---
+++ MvcProject.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcProject.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
