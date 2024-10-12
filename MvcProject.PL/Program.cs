4c7c192a5b61590f992f387e320d716d6be54c00
hamad <hamad@ABDULLAHBAKR>
2024-10-12 20:43:45 +0300

    .

--- MvcProject.PL/Program.cs
+++ MvcProject.PL/Program.cs
@@ -18,7 +18,7 @@ namespace MvcProject.PL
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+             
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
